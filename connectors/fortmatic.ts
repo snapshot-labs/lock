0ec9a1ef0af8774e076aa42e175f7a80885e55f4
adcpm <fabien@bonustrack.co>
2020-07-24 02:02:32 +0700

    Add Fortmatic

---
+++ connectors/fortmatic.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+import Fortmatic from 'fortmatic';
+import LockConnector from '../src/connector';
+
+export default class Connector extends LockConnector {
+  async connect() {
+    let provider;
+    try {
+      const fm: any = new Fortmatic(this.options.apiKey);
+      provider = await fm.getProvider();
+      await fm.user.login();
+      const isLoggedIn = await fm.user.isLoggedIn();
+      if (!isLoggedIn) return;
+    } catch (e) {
+      console.error(e);
+    }
+    return provider;
+  }
+}
