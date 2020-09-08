f759e23a58e343ef095c88d05db0d272e8e7c316
Zhen Yu Yong <fuz3zy@gmail.com>
2020-09-08 12:03:26 +0800

    added torus connector

---
+++ connectors/torus.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+const get = () => import(/* webpackChunkName: "torus" */ 'torus'); // v^2.0.6
+import LockConnector from '../src/connector';
+
+export default class Connector extends LockConnector {
+  async connect() {
+    let provider;
+    try {
+      const Torus = (await get()).default;
+      const torus: any = new Torus();
+      await torus.init({showTorusButton: false});
+      await torus.login(); 
+      provider = torus.provider;
+      return;
+    } catch (e) {
+      console.error(e);
+    }
+    return provider;
+  }
+}
