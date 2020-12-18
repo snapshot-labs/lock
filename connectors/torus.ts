acaa95c6a610e2c01d2ca5e232b8fd5174643199
Yong Zhen Yu <fuz3zy@gmail.com>
2020-12-19 03:08:21 +0800

    added torus connector (#2)
    * added torus connector
    
    * Update connectors/torus.ts
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    
    * added package and version
    
    * rename logo"
    
    * added proxy and other changes
    
    * removed commited package
    
    * torus connector
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    Co-authored-by: chaitanyapotti <chaitanya.potti@gmail.com>

---
+++ connectors/torus.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+const get = () => import(/* webpackChunkName: "torus" */ '@toruslabs/torus-embed'); // v^1.8.5
+import LockConnector from '../src/connector';
+
+export default class Connector extends LockConnector {
+  async connect() {
+    let provider;
+    try {
+      const Torus = (await get()).default;
+      const torus = new Torus({});
+      await torus.init({ showTorusButton: false });
+      await torus.login({});
+      provider = torus.provider
+    } catch (e) {
+      console.error(e);
+    }
+    return provider;
+  }
+}
