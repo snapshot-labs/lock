f6e819ef6b535d13123faa1cc43b026941162abc
Sam <51686767+samuveth@users.noreply.github.com>
2021-01-02 14:35:08 +0700

    feat: vue 3 compatibility (#5)
    * feat: vue 3 compatibility
    
    * feat: support for vue2 and vue3
    
    * fix: ts error

---
+++ plugins/vue3.ts
@@ -0,0 +1,71 @@
+// @ts-ignore
+import { markRaw, ref } from "vue";
+import Lock from "../src/lock";
+
+const name = "lock";
+
+let instance;
+
+export const getInstance = () => instance;
+
+export const useLock = ({ ...options }) => {
+  if (instance) return instance;
+
+  const isAuthenticated = ref(false);
+  const provider = ref(null);
+
+  const lockClient = new Lock();
+  options.connectors.forEach((connector) => {
+    lockClient.addConnector(connector);
+  });
+
+  async function login(connector) {
+    const lockConnector = lockClient.getConnector(connector);
+    const localProvider = await lockConnector.connect();
+    if (localProvider !== null) {
+      localStorage.setItem(`_${name}.connector`, connector);
+      isAuthenticated.value = true;
+      // @ts-ignore
+      provider.value = markRaw(localProvider);
+    }
+    return provider;
+  }
+
+  async function logout() {
+    const connector = await getConnector();
+    if (connector) {
+      const lockConnector = lockClient.getConnector(connector);
+      await lockConnector.logout();
+      localStorage.removeItem(`_${name}.connector`);
+      isAuthenticated.value = false;
+      provider.value = null;
+    }
+  }
+
+  async function getConnector() {
+    const connector = localStorage.getItem(`_${name}.connector`);
+    if (connector) {
+      const lockConnector = lockClient.getConnector(connector);
+      const isLoggedIn = await lockConnector.isLoggedIn();
+      return isLoggedIn ? connector : false;
+    }
+    return false;
+  }
+
+  instance = {
+    isAuthenticated,
+    provider,
+    lockClient,
+    login,
+    logout,
+    getConnector,
+  };
+
+  return instance;
+};
+
+export const LockPlugin = {
+  install(app, options) {
+    app.config.globalProperties.$auth = useLock(options);
+  },
+};
