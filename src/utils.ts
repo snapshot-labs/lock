c1cae4e23fb46c2e0f80894ab61370d662547cc6
Chaitanya <yourchaitu@gmail.com>
2022-02-14 18:01:38 +0530

    Move injected images to IPFS

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,14 @@
 export function getInjected() {
   const web3: any = window['ethereum'];
   if (!web3) return;
-  let injected = { name: 'Injected', id: 'web3' };
-  if (web3.isMetaMask) injected = { name: 'MetaMask', id: 'metamask' };
-  if (web3.isTrust) injected = { name: 'Trust Wallet', id: 'trustwallet' };
-  if (web3.isStatus) injected = { name: 'Status', id: 'status' };
-  if (web3.isFrame) injected = { name: 'Frame', id: 'frame' };
-  if (web3.isAlphaWallet) injected = { name: 'AlphaWallet', id: 'alphawallet' };
-  if (web3.isImToken) injected = { name: 'imToken', id: 'imtoken' };
-  if (web3.isTokenPocket) injected = { name: 'TokenPocket', id: 'tokenpocket' };
-  if (web3.isBitpie) injected = { name: 'Bitpie', id: 'bitpie' };
+  let injected = { name: 'Injected', id: 'web3', icon: 'ipfs://QmXUov1JMszHkizCf3HvmcKWKm9PrG2KHpd5bDnE5YbZN8' };
+  if (web3.isMetaMask) injected = { name: 'MetaMask', id: 'metamask', icon: 'ipfs://QmTE7VPXMhriKAobMWEiC5S3oG22p4G6AXGyGdNWQTQ3Fv' };
+  if (web3.isTrust) injected = { name: 'Trust Wallet', id: 'trustwallet', icon: 'ipfs://QmQzchPwEa7UnRqSS7kSt4EJWMw5mcj9u3YtTp9uig9UT5' };
+  if (web3.isStatus) injected = { name: 'Status', id: 'status', icon: 'ipfs://QmWQhPEvpEH3xW8wwuTr9G5vsUz8ufy25dqe394UJzwsXE' };
+  if (web3.isFrame) injected = { name: 'Frame', id: 'frame', icon: 'ipfs://QmReuKRvC7YTTEmW521nKJwEMFuocWgM9GYwUFedF6Q1BL' };
+  if (web3.isAlphaWallet) injected = { name: 'AlphaWallet', id: 'alphawallet', icon: 'ipfs://QmT7mrsAgpu4V2UJAukaEU9V6fEWYHAo7aahUxfyDdBns9' };
+  if (web3.isImToken) injected = { name: 'imToken', id: 'imtoken', icon: 'ipfs://QmacaenFbh6WXfoPVWzXiijRC8gcBT8N6yt4ZsdMWTWX5J' };
+  if (web3.isTokenPocket) injected = { name: 'TokenPocket', id: 'tokenpocket', icon: 'ipfs://QmSetXfvpWqZVCzh23XQthPFYNQTPAfJaZbkHbWdDdCHzx' };
+  if (web3.isBitpie) injected = { name: 'Bitpie', id: 'bitpie', icon: 'ipfs://QmVUZ8gznsZ2nNv85GFZuTQj31YenyUi5K4HGfhXB3jrAF' };
   return injected;
 }
