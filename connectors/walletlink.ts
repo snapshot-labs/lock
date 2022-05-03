ff7d96c1cd8d7b2f6a1ded9c4ebd48ed5cd2d5c8
midgerate <midgerate@gmail.com>
2022-05-03 11:47:45 +0530

    use wallet-sdk instead of walletlink

--- connectors/walletlink.ts
+++ connectors/walletlink.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
-const get = () => import(/* webpackChunkName: "walletlink" */ 'walletlink'); // v^2.0.2
+const get = () => import(/* webpackChunkName: "walletlink" */ '@coinbase/wallet-sdk'); // v^2.0.2
 import LockConnector from '../src/connector';
 
 export default class Connector extends LockConnector {
   async connect() {
     let provider;
     try {
-      let WalletLink = (await get()).default;
+      let CoinbaseWalletSDK = (await get()).default;
        // @ts-ignore
-      if (WalletLink.default) WalletLink = WalletLink.default;
-      const walletLink = new WalletLink(this.options);
-      provider = walletLink.makeWeb3Provider(
+      if (CoinbaseWalletSDK.default) CoinbaseWalletSDK = CoinbaseWalletSDK.default;
+      const walletSDK = new CoinbaseWalletSDK(this.options);
+      provider = walletSDK.makeWeb3Provider(
         this.options.ethJsonrpcUrl,
         this.options.chainId
       );
