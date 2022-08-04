a9cc7ec43e62b0e18f5a343eec6f8a0bc839f753
Sam <samuv.eth@gmail.com>
2022-08-04 13:03:39 +0700

    Fix walletconnect

--- connectors/walletconnect.ts
+++ connectors/walletconnect.ts
@@ -1,22 +1,22 @@
-const get = () => import(/* webpackChunkName: "walletconnect" */ '@walletconnect/web3-provider');
 import LockConnector from '../src/connector';
 
 export default class Connector extends LockConnector {
   async connect() {
-    let provider;
+  let provider;
     try {
-      let WalletConnectProvider = (await get()).default;
-      if (WalletConnectProvider.default)
-        WalletConnectProvider = WalletConnectProvider.default;
-      provider = new WalletConnectProvider(this.options);
+      const WalletConnectProvider = await import(
+        "@walletconnect/web3-provider/dist/umd/index.min.js"!
+      );
+      provider = new WalletConnectProvider.default(this.options);
       await provider.enable();
     } catch (e) {
       console.error(e);
       return;
     }
-    provider.connectorName = 'walletconnect';
+    provider.connectorName = "walletconnect";
     return provider;
   }
+
 
   logout() {
     if (localStorage) {
